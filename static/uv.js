31cc60063ed96ef01cfd97b4688b92e5339ec6b8
LiprodCodez <turtle.script.dev@gmail.com>
2025-02-02 13:19:36 -0500

    Update uv.js

--- static/uv.js
+++ static/uv.js
@@ -4,28 +4,5 @@ importScripts('/static/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
-.
-self.addEventListener('install', (event) => {
-console.log('[Service Worker] Installing...');
-self.skipWaiting();
-});
-self.addEventListener('activate', (event) => {
-console.log('[Service Worker] Activating...');
-event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', (event) => {
-event.respondWith(
-(async () => {
-try {
-const response = await sw.fetch(event);
-return response;
-} catch (error) {
-console.error('[Service Worker] Fetch error:', error);
-return new Response('Service Unavailable', {
-status: 503,
-statusText: 'Service Unavailable'
-        });
-      }
-    })()
-  );
-});
+
+self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
